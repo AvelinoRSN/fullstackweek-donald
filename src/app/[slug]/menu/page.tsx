4aedecaea3a283e3131bdcf896c599b3a913dbb5
Avelino Ramos <avelinorsilvaneto@gmail.com>
2025-02-22 19:41:56 -0300

    feat: add restaurant menu page with categories and products

--- src/app/[slug]/menu/page.tsx
+++ src/app/[slug]/menu/page.tsx
@@ -2,6 +2,7 @@ import { notFound } from "next/navigation";
 
 import { db } from "@/lib/prisma";
 
+import RestaurantCategories from "./components/categories";
 import RestaurantHeader from "./components/header";
 
 interface RestaurantMenuPageProps {
@@ -23,7 +24,15 @@ const RestaurantMenuPage = async ({
   if (!isConsumptionMethodValid(consumptionMethod)) {
     return notFound();
   }
-  const restaurant = await db.restaurant.findUnique({ where: { slug: slug } });
+  const restaurant = await db.restaurant.findUnique({
+    where: { slug: slug },
+    include: {
+      menuCatergories: {
+        include: { products: true },
+      },
+    },
+  });
+
   if (!restaurant) {
     return notFound();
   }
@@ -31,6 +40,7 @@ const RestaurantMenuPage = async ({
   return (
     <div>
       <RestaurantHeader restaurant={restaurant} />
+      <RestaurantCategories restaurant={restaurant} />
     </div>
   );
 };
